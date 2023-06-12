8a0013596d67f598e8fbce76bd7c9888f5dfd552
praneet9 <praneetbomma@gmail.com>
2023-06-12 18:01:10 +0100

    added callbacks

--- trainer.py
+++ trainer.py
@@ -2,6 +2,7 @@ from classifier import Classifier
 from dataset import ImageClassificationDataset
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 import config as cfg
 
 model = Classifier(cfg.N_CLASSES, cfg.LR)
@@ -24,5 +25,13 @@ trainer = pl.Trainer(
     max_epochs=cfg.N_EPOCHS,
     precision=cfg.PRECISION,
     fast_dev_run=cfg.DEV_MODE,
+    callbacks=[EarlyStopping(monitor='Validation Loss', patience=5),
+               ModelCheckpoint(
+                   dirpath='model_checkpoints',
+                   filename='{epoch}-{Validation Loss:.2f}-{Validation Accuracy:.2f}',
+                   save_last=True,
+                   monitor='Validation Accuracy',
+                   mode='max'
+			   )]
 )
 trainer.fit(model, train_dataloader, test_dataloader)
